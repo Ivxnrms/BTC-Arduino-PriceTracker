0be7ab35c618e4ed66bbc79d77d65d1b7a8fafd0
Iván Ramos <74296930+Ivxn37@users.noreply.github.com>
2024-05-23 10:53:59 +0200

    Update Codigo.ino

--- Codigo.ino
+++ Codigo.ino
@@ -19,7 +19,7 @@ const String url = "http://api.coindesk.com/v1/bpi/currentprice.json";
 const String historyURL = "http://api.coindesk.com/v1/bpi/historical/close.json";
 const String cryptoCode = "BTC";
 
-// Logo del Bitcoin
+// Icono BTC
 const unsigned char bitcoinIcon [] PROGMEM = {
 0x00, 0x7e, 0x00, 0x03, 0xff, 0xc0, 0x07, 0x81, 0xe0, 0x0e, 0x00, 0x70, 0x18, 0x28, 0x18, 0x30, 
 0x28, 0x0c, 0x70, 0xfc, 0x0e, 0x60, 0xfe, 0x06, 0x60, 0xc7, 0x06, 0xc0, 0xc3, 0x03, 0xc0, 0xc7, 
@@ -54,10 +54,10 @@ void setup() {
     delay(500);
   }
 
-  Serial.print("CONECTADO A: ");
+  
   Serial.println(ssid);
 
-  display.print("Conectado a ");
+  
   display.println(ssid);
   display.display();
   delay(5000);
@@ -99,7 +99,7 @@ void loop() {
       String lastUpdated = doc["time"]["updated"].as<String>();
       http.end();
 
-      Serial.println("Obteniendo el historial...");
+      Serial.println("Obteniendo historial...");
       StaticJsonDocument<1536> historyDoc;
       http.begin(wificlient, historyURL);
       int historyHttpCode = http.GET();
@@ -112,29 +112,19 @@ void loop() {
         return;
       }
 
-
+      
       display.clearDisplay();
       display.drawBitmap((128/2) - (24/2), 0, bitcoinIcon, 24, 24, WHITE);
       display.display();
 
-      
-      display.setTextSize(2);
-      printCenter("$" + BTCUSDPrice, 0, 32);
+ //Precio BTC
+ display.setTextSize(1);
+ printCenter("$" + BTCUSDPrice, 0, 32);
 
-      
-      double yesterdayPrice = historyDoc["bpi"]["2021-03-20"].as<double>();
-      bool isUp = BTCUSDPrice.toDouble() > yesterdayPrice;
-      double percentChange;
-      String dayChangeString = "24hr. Cambio: ";
-      if (isUp) {
-        percentChange = ((BTCUSDPrice.toDouble() - yesterdayPrice) / yesterdayPrice) * 100;
-      } else {
-        percentChange = ((yesterdayPrice - BTCUSDPrice.toDouble()) / yesterdayPrice) * 100;
-        dayChangeString = dayChangeString + "-";
-      }
-      display.setTextSize(1);
-      dayChangeString = dayChangeString + percentChange + "%";
-      printCenter(dayChangeString, 0, 55);
+ //Firma
+ display.setTextSize(1);
+ display.setCursor(5, 55);
+ display.println("@ivxn.ramos_ - RYC");
 
       display.display();
       http.end();
@@ -147,7 +137,7 @@ void printCenter(const String buf, int x, int y)
 {
   int16_t x1, y1;
   uint16_t w, h;
-  display.getTextBounds(buf, x, y, &x1, &y1, &w, &h); 
+  display.getTextBounds(buf, x, y, &x1, &y1, &w, &h); //calc width of new string
   display.setCursor((x - w / 2) + (128 / 2), y);
   display.print(buf);
 }
